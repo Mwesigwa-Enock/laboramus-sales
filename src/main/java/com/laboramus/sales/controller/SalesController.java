84631dd9f274271464b5da66073cc8f65005af50
PAVANS <enockmwesigwa7@gmail.com>
2020-10-18 15:49:30 +0300

    cors

--- src/main/java/com/laboramus/sales/controller/SalesController.java
+++ src/main/java/com/laboramus/sales/controller/SalesController.java
@@ -24,7 +24,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/sales")
 public class SalesController {
